18d2bd37ca232362103737208bae5b55e0c461b1
Angelika <anzhelika48@mail.ru>
2022-09-17 16:27:47 +0500

    Решила 56 задачу

---
+++ Zadacha_56/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Console.WriteLine("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[rows,columns];
+int temp = int.MaxValue;
+int k = 0;
+int sum = 0;
+
+Console.WriteLine("Массив:");
+
+if (rows != columns)
+{
+  for(int i=0; i<array.GetLength(0); i++)
+  {
+     for(int j=0; j<array.GetLength(1); j++)
+     {
+      array[i, j] = new Random().Next(0, 10);
+      Console.Write(array[i, j] + "\t");
+
+     }
+   Console.WriteLine();
+   }
+
+   for(int i=0; i<array.GetLength(0); i++)
+   {
+     for(int j=0; j<array.GetLength(1); j++)
+     {
+     sum += array[i,j]; 
+     }
+   Console.WriteLine("Сумма элементов строки равна:" + sum);
+     if (temp > sum)
+     {
+     temp = sum;
+     k = i;
+     }
+   }  
+   Console.WriteLine("Номер строки с наименьшей суммой элментов :" + k);
+}
+else
+{
+  Console.WriteLine("Массив не прямоугольный");
+}
