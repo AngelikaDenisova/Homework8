9b2f7321521289dfcb03fb22b6d39a1c12ea878b
Angelika <anzhelika48@mail.ru>
2022-09-17 16:26:10 +0500

    Решила 54 задачу

---
+++ Zadacha_54/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] array = new int[new Random().Next(1, 10), new Random().Next(1, 10)];
+
+Console.WriteLine("Первоначальный массив:");
+
+for(int i=0; i<array.GetLength(0); i++)
+{
+   for(int j=0; j<array.GetLength(1); j++)
+   {
+    array[i, j] = new Random().Next(0, 10);
+    Console.Write(array[i, j] + "\t");
+
+   }
+Console.WriteLine();
+}
+
+Console.WriteLine("Упорядочили по убыванию:");
+
+
+for(int i=0; i<array.GetLength(0); i++)
+{
+   for(int j=0; j<array.GetLength(1); j++)
+   {
+    for(int k = 0; k < array.GetLength(1)-1; k ++)
+    {
+      if(array[i,k+1] > array[i, k])
+      {
+         int temp = array[i,k];
+         array[i,k] = array[i,k+1];
+         array[i,k+1] = temp;
+      }
+
+    }
+   Console.Write(array[i,j] + "\t");
+
+   }
+Console.WriteLine();
+}
